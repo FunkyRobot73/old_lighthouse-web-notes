89c0e10c1e12abc460d0bacb6818e24074f84929
FunkyRobot73 <arlo252@gmail.com>
2021-09-16 21:19:22 +0000

    This is my JLA Array

---
+++ week_1/day_1/02_jla_array.js
@@ -0,0 +1,7 @@
+let jlaOriginal = ["batman", "superman", "wonder woman", "flash", "green lantern", "aquaman", "martian manhunter"];
+let jlaSatellite = ["green arrow", "atom", "hawkman", "hawkgirl", "black canary", "elongated man", "red tornado", "zatanna", "firestorm"]
+let jlaDetroit = [jlaOriginal[6], jlaOriginal[0], jlaSatellite[5], jlaOriginal[5], "vixen", "vibe", "vteel", "vypsy"];
+let jliOriginal = [jlaOriginal[6], jlaOriginal[0], jlaSatellite[4], "blue beetle", "dr. fate", "captain marvel", "dr. light", "mr. miracle", "oberon", "guy gardner", "booster gold"];
+
+console.log(jliOriginal);
+
